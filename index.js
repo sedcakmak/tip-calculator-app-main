00d70c18d3faa04a1b89c9c8ee4289e43db33d74
Sedef Cakmak <cakmaksedef@gmail.com>
2022-07-26 03:07:52 -0400

    added grids

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const people = document.getElementById("people");
+const error = document.querySelector(".error");
 
+people.addEventListener("input", updateValue);
+
+function updateValue(e) {
+  if (e.target.value < 1 && e.target.value !== "") {
+    error.style.display = "inline";
+    people.classList.add("invalid");
+  } else {
+    error.style.display = "none";
+    people.classList.remove("invalid");
+  }
+}
