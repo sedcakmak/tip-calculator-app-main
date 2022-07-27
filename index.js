27acd602720ae3f087481397f31a40411eb0e32d
Sedef Cakmak <cakmaksedef@gmail.com>
2022-07-27 00:52:34 -0400

    added event listeners

--- index.js
+++ index.js
@@ -1,9 +1,51 @@
+const bill = document.getElementById("bill");
 const people = document.getElementById("people");
 const error = document.querySelector(".error");
+const tipButtons = document.querySelectorAll(".tip-button");
+const customTip = document.getElementById("custom-tip");
+const total = document.getElementById("total");
 
-people.addEventListener("input", updateValue);
+bill.addEventListener("input", getValue);
 
-function updateValue(e) {
+document.body.addEventListener("change", testing);
+
+function testing(e) {
+  let tipDisplay;
+  let totalDisplay;
+
+  console.log(e.target.id + " and value is " + e.target.value);
+}
+
+//tip amount = bill *  select-tip.value /100 / people.value
+//total = (bill + tip amount*people) / people
+
+people.addEventListener("input", (e) => {
+  //   getValue(e);
+  errorMessage(e);
+});
+
+// tipButtons.forEach((button) => {
+//   button.addEventListener("click", activeClass);
+// });
+tipButtons.forEach((button) => {
+  button.addEventListener("click", setChecked);
+});
+
+function setChecked(e) {
+  e.target.checked = true;
+}
+
+// const test = customTip.addEventListener("input", getValue);
+let value;
+function getValue(e) {
+  typeof e.target.value === "string"
+    ? (value = Number(e.target.value))
+    : (value = e.target.value);
+
+  return value;
+}
+
+function errorMessage(e) {
   if (e.target.value < 1 && e.target.value !== "") {
     error.style.display = "inline";
     people.classList.add("invalid");
@@ -11,4 +53,10 @@ function updateValue(e) {
     error.style.display = "none";
     people.classList.remove("invalid");
   }
+}
+
+function activeClass(e) {
+  e.target.classList.contains("active")
+    ? e.target.classList.remove("active")
+    : e.target.classList.add("active");
 }
