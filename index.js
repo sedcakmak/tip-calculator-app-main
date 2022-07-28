60f2dfd75ef08d73fb996dd9c3416d2d6087d4a9
Sedef Cakmak <cakmaksedef@gmail.com>
2022-07-28 14:45:42 -0400

    implemented calculation in js

--- index.js
+++ index.js
@@ -4,23 +4,57 @@ const error = document.querySelector(".error");
 const tipButtons = document.querySelectorAll(".tip-button");
 const customTip = document.getElementById("custom-tip");
 const total = document.getElementById("total");
+const tipAmount = document.getElementById("total-tip");
+//const fields = document.querySelectorAll(".validated-field");
 
-bill.addEventListener("input", getValue);
+//bill.addEventListener("input", getValue);
 
-document.body.addEventListener("change", testing);
+document.body.addEventListener("input", calculate);
 
-function testing(e) {
+function calculate(e) {
   let tipDisplay;
   let totalDisplay;
 
-  console.log(e.target.id + " and value is " + e.target.value);
+  if (document.querySelector('input[name="buttonGroup"]:checked')) {
+    tipDisplay =
+      (bill.value *
+        Number(
+          document.querySelector('input[name="buttonGroup"]:checked').value
+        )) /
+      100;
+  } else {
+    tipDisplay = (bill.value * Number(customTip.value)) / 100;
+  }
+
+  tipDisplay
+    ? (totalDisplay = Number(bill.value) + tipDisplay)
+    : (totalDisplay = Number(bill.value));
+
+  if (people.value > 0) {
+    total.innerHTML = "$" + (totalDisplay / Number(people.value)).toFixed(2);
+    tipAmount.innerHTML = "$" + (tipDisplay / Number(people.value)).toFixed(2);
+  } else {
+    total.innerHTML = "$" + totalDisplay.toFixed(2);
+    tipAmount.innerHTML = "$" + tipDisplay.toFixed(2);
+  }
+
+  // console.log(e.target.id + "" + e.target.value);
+  // console.log("bill value " + bill.value);
+  // console.log("people " + people.value);
+  // console.log("custom-tip " + customTip.value);
+  // console.log("bill" + typeof Number(bill.value));
+  // console.log("people" + typeof people.value);
+  // console.log("tip" + typeof customTip.value);
+  //console.log(e.target.id + " and value is " + e.target.value);
 }
 
 //tip amount = bill *  select-tip.value /100 / people.value
 //total = (bill + tip amount*people) / people
 
+// var tipValue = bill * (tip/100)
+// var finalBill = bill + tipValue
+
 people.addEventListener("input", (e) => {
-  //   getValue(e);
   errorMessage(e);
 });
 
@@ -32,31 +66,31 @@ tipButtons.forEach((button) => {
 });
 
 function setChecked(e) {
+  if (customTip.value) customTip.value = "";
   e.target.checked = true;
 }
 
-// const test = customTip.addEventListener("input", getValue);
-let value;
-function getValue(e) {
-  typeof e.target.value === "string"
-    ? (value = Number(e.target.value))
-    : (value = e.target.value);
+customTip.addEventListener("input", uncheckTipButtons);
 
-  return value;
+function uncheckTipButtons() {
+  if (document.querySelector('input[name="buttonGroup"]:checked')) {
+    document.querySelector('input[name="buttonGroup"]:checked').checked = false;
+  }
+}
+
+function getValue(e) {
+  console.log(
+    "getValue working " + Number(e.target.value) + typeof Number(e.target.value)
+  );
+  return Number(e.target.value);
 }
 
 function errorMessage(e) {
   if (e.target.value < 1 && e.target.value !== "") {
     error.style.display = "inline";
-    people.classList.add("invalid");
+    people.classList.add("invalid-value");
   } else {
     error.style.display = "none";
-    people.classList.remove("invalid");
+    people.classList.remove("invalid-value");
   }
-}
-
-function activeClass(e) {
-  e.target.classList.contains("active")
-    ? e.target.classList.remove("active")
-    : e.target.classList.add("active");
 }
