6c11b1322c47ed660303316585131f4d75cfba35
Sedef Cakmak <cakmaksedef@gmail.com>
2022-07-28 16:10:56 -0400

    implemented reset button

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 const bill = document.getElementById("bill");
-const people = document.getElementById("people");
-const error = document.querySelector(".error");
 const tipButtons = document.querySelectorAll(".tip-button");
 const customTip = document.getElementById("custom-tip");
+const people = document.getElementById("people");
+const error = document.querySelector(".error");
 const total = document.getElementById("total");
 const tipAmount = document.getElementById("total-tip");
-//const fields = document.querySelectorAll(".validated-field");
+const resetButton = document.querySelector(".reset");
 
-//bill.addEventListener("input", getValue);
+document.body.addEventListener("input", () => {
+  calculate();
+  activateResetButton();
+});
 
-document.body.addEventListener("input", calculate);
+tipButtons.forEach((button) => {
+  button.addEventListener("click", setChecked);
+});
 
-function calculate(e) {
+customTip.addEventListener("input", uncheckTipButtons);
+
+people.addEventListener("input", (e) => {
+  errorMessage(e);
+});
+
+//resetButton.addEventListener("click", resetAllInput);
+
+function calculate() {
   let tipDisplay;
   let totalDisplay;
 
@@ -37,52 +50,21 @@ function calculate(e) {
     total.innerHTML = "$" + totalDisplay.toFixed(2);
     tipAmount.innerHTML = "$" + tipDisplay.toFixed(2);
   }
-
-  // console.log(e.target.id + "" + e.target.value);
-  // console.log("bill value " + bill.value);
-  // console.log("people " + people.value);
-  // console.log("custom-tip " + customTip.value);
-  // console.log("bill" + typeof Number(bill.value));
-  // console.log("people" + typeof people.value);
-  // console.log("tip" + typeof customTip.value);
-  //console.log(e.target.id + " and value is " + e.target.value);
 }
 
-//tip amount = bill *  select-tip.value /100 / people.value
-//total = (bill + tip amount*people) / people
-
-// var tipValue = bill * (tip/100)
-// var finalBill = bill + tipValue
-
-people.addEventListener("input", (e) => {
-  errorMessage(e);
-});
-
-// tipButtons.forEach((button) => {
-//   button.addEventListener("click", activeClass);
-// });
-tipButtons.forEach((button) => {
-  button.addEventListener("click", setChecked);
-});
+function activateResetButton() {
+  resetButton.disabled = false;
+}
 
 function setChecked(e) {
   if (customTip.value) customTip.value = "";
   e.target.checked = true;
 }
 
-customTip.addEventListener("input", uncheckTipButtons);
-
 function uncheckTipButtons() {
   if (document.querySelector('input[name="buttonGroup"]:checked')) {
     document.querySelector('input[name="buttonGroup"]:checked').checked = false;
   }
-}
-
-function getValue(e) {
-  console.log(
-    "getValue working " + Number(e.target.value) + typeof Number(e.target.value)
-  );
-  return Number(e.target.value);
 }
 
 function errorMessage(e) {
@@ -93,4 +75,9 @@ function errorMessage(e) {
     error.style.display = "none";
     people.classList.remove("invalid-value");
   }
+}
+
+function resetAllInput() {
+  document.getElementById("myForm").reset();
+  //window.location.reload();
 }
